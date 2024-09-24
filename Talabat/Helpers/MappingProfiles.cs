5fee363db0de77ce271431ae8305c96588bc7f5e
Riham <rihammohareb6@gmail.com>
2024-09-24 23:22:54 +0300

    Product PictureUrl Resolver

--- Talabat/Helpers/MappingProfiles.cs
+++ Talabat/Helpers/MappingProfiles.cs
@@ -6,11 +6,15 @@ namespace Talabat.Helpers
 {
     public class MappingProfiles:Profile
     {
+       
+
         public MappingProfiles()
         {
+           
             CreateMap <Product ,ProductToReturnDto > ().ForMember (d=>d.Brand ,o=>o.MapFrom (s=>s.Brand .Name )).ForMember (
-                d=>d.Category,o=>o.MapFrom (s=>s.Category .Name ));
-
+                d=>d.Category,o=>o.MapFrom (s=>s.Category .Name )).ForMember (
+                d=>d.PictureUrl ,o=>o.MapFrom<PictureUrlResolver >());
+         
         }
 
     }
---
+++ Talabat/Helpers/PictureUrlResolver.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using AutoMapper.Execution;
+using Talabat.Core.Entities;
+using Talabat.DTOs;
+
+namespace Talabat.Helpers
+{
+    public class PictureUrlResolver:IValueResolver<Product,ProductToReturnDto ,string>
+    {
+        private readonly IConfiguration _configration;
+
+        public PictureUrlResolver(IConfiguration configration)
+        {
+            _configration = configration;
+        }
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            
+                return $"{_configration["ApiBaseUrl"]}/{source .PictureUrl }";
+
+            return string.Empty;
+
+          
+        }
+    }
+}
